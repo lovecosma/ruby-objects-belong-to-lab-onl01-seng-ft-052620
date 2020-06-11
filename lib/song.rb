0e0ed189c3c35b248ff1c8660ddb6edd147e037e
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-11 16:19:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,8 @@
 class Song
 attr_accessor :title, :artist
-def initialize(title = "", author = "")
+def initialize(title = "", artist = "")
   @title = title
-  @author = author
+  @author = artist
 end
 
 
